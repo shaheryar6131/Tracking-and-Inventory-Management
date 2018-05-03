67efb334a1f27f61cca36bea1eabfbf1a7fc08e3
Rishi Parikh <rishiparikh2001@gmail.com>
2018-05-03 09:38:29 -0700

    Update WareHouse.java

--- WareHouse.java
+++ WareHouse.java
@@ -1,7 +1,7 @@
 import lib.Sort;
 import lib.Search;
 public class WareHouse extends Storage, implements Shippable{
-  private ArrayList<Storage> hubs; = new ArrayList<Storage>;
+  private ArrayList<Storage> hubs = new ArrayList<Storage>;
   private int size = Constants.largeStorageMax;
   public WareHouse(ArrayList<ShippingFacilites> a, ArrayList<Items> b, int xCord, int yCord, int red, int yellow, int max){
     super(a, xCord, yCord, red, yellow, max);
