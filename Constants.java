02f7e0863602b65cdbe006934dfae1a4325b878a
rkolli0603 <38920181+rkolli0603@users.noreply.github.com>
2018-05-07 09:42:30 -0700

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,9 +1,9 @@
 public class Constants{
   int smallStorageMax = 100;
-  int[] location A1 = {100,100};
-  int[] location A2 = {200,100};
-  int[] location A2 = {300,100};
-  int[] location A2 = {400,100};
+  int[] locationA1 = {100,100};
+  int[] locationA2 = {200,100};
+  int[] locationA3 = {300,100};
+  int[] locationA4 = {400,100};
   int largeStorageMax = 300;
   int[] wareHouseLocation = {0,0};
   int redAlert = 25;
