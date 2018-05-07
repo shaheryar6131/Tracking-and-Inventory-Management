f0f11715b83ba920e47fa480f6aee21177687432
rkolli0603 <38920181+rkolli0603@users.noreply.github.com>
2018-05-07 10:03:57 -0700

    Update Storage.java

--- Storage.java
+++ Storage.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
-
+/*
+ * Storage Class
+ * Class to store items and keep track of quantity of items
+ */
 public class Storage implements Shippable{
   int maxCapacity;
   boolean redAlert;
@@ -9,6 +12,15 @@ public class Storage implements Shippable{
   int redLine;
   int yellowLine;
   ArrayList<Item> myInv = new ArrayList<Item>();
+  /*
+   * Constructor method for Storage class
+   * @param a = array list of items that will be shipped
+   * @param xCord = x coordinate of the location of our item
+   * @param yCord = y coordinate of the location of our item
+   * @param red = red shipping line 
+   * @param yellow = yellow shipping line
+   * @param max = max number of items storage can hold
+   */
   public Storage(ArrayList<Item> a, int xCord, int yCord, int red, int yellow, int max){
     myInv = a;
     x = xCord;
@@ -19,12 +31,22 @@ public class Storage implements Shippable{
     yellowAlert = checkYellowAlert() != -1;
     maxCapacity = max;
   }
+  /*
+   * Transport method
+   * Used to transport items from the storage unit to a specific location
+   * @param a = Item to be shipped
+   * @param numItems = number of items to be shipped
+   * @param destination = desired destination for specific item
+   */
   public void transportItem(Item a, int numItems, Storage destination){
     int itemNumber = search(myInv, a);
     myInv.get(itemNumber).setNumItems(myInv.get(itemNumber).getNumItems() - numItems);
     int recieveItemNumber = search(destination.myInv, a);
     destination.myInv.get(itemNumber).setNumItems(destination.myInv.get(itemNumber).getNumItems() + numItems);
   }
+  /*
+   * method to check if there are items that need to be shipped immediately
+   */
   public int checkRedAlert(){
     for(int i = 0; i < myInv.size(); i++){
       if(myInv.get(i).getNumItems() < redLine){
@@ -33,6 +55,9 @@ public class Storage implements Shippable{
     }
     return -1;
   }
+  /*
+   * Method to check if there items that need to be shipped on a yellow line
+   */
   public int checkYellowAlert(){
     if(redAlert){
       return -1;
@@ -44,15 +69,25 @@ public class Storage implements Shippable{
     }
     return -1;
   }
+  /*
+   * Gets an update from the status of our shippable project
+   */
   public boolean hasUpdate(){
     if(redAlert || yellowAlert){
       return true;
     }
     return false;
   }
+  /*
+   * Method to add an item into our array
+   * @param a = array of items in storage
+   */
   public void addInv(Item a){
     myInv.add(a);
   }
+  /*
+   * Method to get location of a specific item
+   */
   public int[] getLocation(){
     int[] a = new int[2];
     a[0] = x;
