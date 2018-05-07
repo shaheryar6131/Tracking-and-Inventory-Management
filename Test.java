371ff5c8ad7c40f97a3a77bf64ba1cba078f9cc1
rkolli0603 <38920181+rkolli0603@users.noreply.github.com>
2018-05-07 09:44:50 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 public class Test{
   public static void main(String[] args){
     ArrayList<Item> a = new ArrayList<Item>();
-    a.add(new Item("food", 50));
-    a.add(new Item("water", 50));
-    a.add(new Item("stuff", 30));
-    Storage b = new Storage(a, 0, 0, 40, 45);
-    Storage c = new Storage(a, 0, 0, 40, 45);
+    a.add(new Item("food", 50, 0));
+    a.add(new Item("water", 50, 0));
+    a.add(new Item("stuff", 30, 0));
+    Storage b = new Storage(a, 0, 0, 40, 45, 0);
+    Storage c = new Storage(a, 0, 0, 40, 45, 0);
     System.out.println(b.checkRedAlert());
   }
 }
